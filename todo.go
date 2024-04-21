ca6826772e791703af5b9a26e5c5e909e8d74a56
Tomoya Nagasawa <ocrybit@gmail.com>
2024-04-21 21:28:47 +0400

    add task

--- todo.go
+++ todo.go
@@ -1,8 +1,35 @@
 package main
 
-import ( "fmt"; "bufio"; "os"; "strings" )
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
 
-func main() {
+type Task struct {
+	name string
+	done bool
+	
+}
+
+var todos []Task
+
+func add() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("enter task: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	words := strings.Split(strings.TrimSpace(input), ",")
+	task := Task{ words[0], false }
+	todos = append(todos, task)
+	fmt.Println(todos)
+}
+
+func command() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("enter command: ")
 	input, err := reader.ReadString('\n')
@@ -15,4 +42,16 @@ func main() {
 	cmd := words[0]
 	
 	fmt.Println("Command:", cmd)
+
+	switch cmd {
+	case "a", "add":
+		add()
+	default:
+		fmt.Println("unknown command:", cmd)
+	}
+	command()
+}
+
+func main() {
+	command()
 }
